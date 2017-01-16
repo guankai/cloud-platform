1c822950333a733fcac6759f2b421c6c0bfe6dce
TangQian <qian.tang@hnair.com>
2017-01-16 22:27:09 +0800

    update kong api update method

--- utils/kong/api.go
+++ utils/kong/api.go
@@ -29,8 +29,15 @@ func ListAPIs(size int, offset string) (*models.APIList, error) {
 	return nil, nil
 }
 
-//UpdateAPI Update API
-func UpdateAPI(nameOrID string) (*models.API, error) {
+// UpdateAPI Update API
+// nameOrID (required) The original unique identifier or the name of the API to update
+// name -- (optional)The API name. If none is specified, will default to the request_host or request_path.
+// request_host -- (semi-optional)The public DNS address that points to your API. For example, mockbin.com. At least request_host or request_path or both should be specified.
+// request_path -- (semi-optional)	The public path that points to your API. For example, /someservice. At least request_host or request_path or both should be specified.
+// strip_request_path -- (optional)	Strip the request_path value before proxying the request to the final API. For example a request made to /someservice/hello will be resolved to upstream_url/hello. By default is false.
+// preserve_host -- (optional)	Preserves the original Host header sent by the client, instead of replacing it with the hostname of the upstream_url. By default is false.
+// upstream_url	The base target URL that points to your API server, this URL will be used for proxying requests. For example, https://mockbin.com.
+func UpdateAPI(nameOrID string, api *models.API) (*models.API, error) {
 	//PATCH /apis/{name or id}
 
 	return nil, nil
